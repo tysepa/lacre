9635e041bb8c53315c1bf0ebf338d1b58e08fff7
Epa <tysepa88@gmail.com>
2025-12-18 14:34:18 +0200

    Update env examoles

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -13,7 +13,7 @@ const center = {
 
 export default function Map() {
   return (
-    <LoadScript googleMapsApiKey=" 5127-7231-9256">
+    <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12} />
     </LoadScript>
   );
